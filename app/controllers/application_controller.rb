332c12d47a3941be77b8372adf70570c4681d76a
Dimitrije Todorovic <dimitrije@umail.ucsb.edu>
2011-07-05 21:08:19 -0700

    stylesheet added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def calculate_angle
+
+  	x/(h*((((a*y - h) + ((a*y-h)**2 + 4*(x**2 + y**2)*(a**2)*(0.25))**0.5))/(2*(x**2 + y**2)))**0.5)
+  	(a*x)/h**2
+
+  end
 end
